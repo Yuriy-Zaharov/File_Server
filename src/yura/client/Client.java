a3fb682b2ab7c9f0b8a50a157956321c466e5d4a
Yuriy-Zakharov <yura0zaharov@gmail.com>
2024-03-12 21:49:44 +0300

    Task2 Ver 0.1

--- src/yura/client/Client.java
+++ src/yura/client/Client.java
@@ -1,10 +1,15 @@
 package yura.client;
 
-import yura.server.Main;
 import yura.server.Server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private Server server;
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
 
     public Client() {
 
@@ -22,6 +27,16 @@ public class Client {
         return server;
     }
 
+    public static String readCommand(Scanner scanInput) {
+        String strInput;
+        try {
+            strInput = scanInput.nextLine();
+        } catch (Exception e) {
+            return "";
+        }
+        return strInput;
+    }
+
     public void handleCommand(String strCommand) {
         String[] commandWords = strCommand.split("\\s+");
 
@@ -31,7 +46,7 @@ public class Client {
         }
         if (commandWords.length < 2) {
             if ("exit".equals(commandWords[0])) {
-                Main.appOn = false;
+                Server.appOn = false;
             } else {
                 System.out.println("File's name wasn't inputted. add usage: add " + server.FILE_NAME + "1");
             }
@@ -51,6 +66,26 @@ public class Client {
             default -> {
                 System.out.println("Command wasn't found.");
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanInput = new Scanner(System.in);
+
+        try (
+            Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+
+            output.writeUTF(msg); // send a message to the server
+            String receivedMsg = input.readUTF(); // read the reply from the server
+
+            System.out.println("Received from the server: " + receivedMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/yura/server/Server.java
+++ src/yura/server/Server.java
@@ -1,15 +1,21 @@
 package yura.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
+    private static final int PORT = 34522;
+    public static boolean appOn = true;
     public final String FILE_NAME = "file";
-    private int countFiles;
     private File[] files;
 
-    Server(int countFiles) {
-        this.countFiles = countFiles;
-        this.files = new File[countFiles];
+    Server() {
+        this.files = new File[5];
     }
 
     public void addFile(String fileName) {
@@ -69,9 +75,29 @@ public class Server {
         } catch (Exception e) {
             return -1;
         }
-        if (fileIndex + 1 > countFiles) {
-            return -1;
-        }
         return fileIndex;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanInput = new Scanner(System.in);
+
+        String address = "127.0.0.1";
+
+        try (ServerSocket server = new ServerSocket(PORT, 50, InetAddress.getByName(address));) {
+            while (true) {
+                try (
+                    Socket socket = server.accept();
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+                ) {
+                    String msg = input.readUTF(); // read a message from the client
+                    output.writeUTF(msg); // resend it to the client
+                    System.out.println(msg);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
